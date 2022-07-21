b2e10384a3ce140344faf0b5080edd611f48d92d
swonl <swonl.mail@gmail.com>
2022-07-21 21:41:02 +0300

    change structure

--- client/src/serialization.ts
+++ client/src/serialization.ts
@@ -2,19 +2,20 @@ import lo from "buffer-layout"
 import BN from "bn.js"
 
 export interface IPData {
-  ip_data: BN[]
+  ip_array: BN[]
 }
 
 export function decodeIPData(data: Buffer): IPData {
   let ipdataSchema = lo.struct([lo.u32("size"), lo.seq(lo.ns64(), (data.length - 4) / 8, "ip_array")])
-  return ipdataSchema.decode(data).ip_array
+  return ipdataSchema.decode(data)
 }
 
 export function encodeIPData(
-  ip_data: Array<BN>
+  ip_data: IPData
 ): Buffer {
-  const schema = lo.struct([lo.u32("size"), lo.seq(lo.ns64(), ip_data.length, "ip_array")])
-  const b = Buffer.alloc(4 + 8 * ip_data.length)
-  schema.encode({size: ip_data.length, ip_array: ip_data }, b)
+  const ip_array = ip_data.ip_array
+  const schema = lo.struct([lo.u32("size"), lo.seq(lo.ns64(), ip_array.length, "ip_array")])
+  const b = Buffer.alloc(4 + 8 * ip_array.length)
+  schema.encode({size: ip_array.length, ip_array: ip_array }, b)
   return Buffer.from(b)
 }
